0d2cb81edacebf6ec1ba17f20700743e7e6e305e
Darwin Felipe Uzcategui Portatil Lenovo <darwin.uzcategui1973@hotmail.com>
2019-03-13 20:55:20 -0400

    prueba  de curso vscode

---
+++ src/app/03-clases-definiciones/04-replace-definition.ts
@@ -0,0 +1,25 @@
+import { Heroes } from './extra/classes';
+
+/*
+    Objetivo:
+        Cambiar únicamente la refencia de SuperHeroe a Heroe
+        OJO!: Pero no reemplazar los strings
+
+    Tips:
+        Replace Symbol
+        F2
+*/
+
+
+const wolverine = new Heroes();
+const ironman   = new Heroes();
+const spiderman = new Heroes();
+
+function saludar() {
+    return 'El SuperHeroe Wolverine es genial!';
+}
+
+function gritar() {
+    return 'El SuperHeroe en este string no se debe de cambiar';
+}
+
---
+++ src/app/03-clases-definiciones/extra/classes.ts
@@ -0,0 +1,20 @@
+export class Heroes {
+
+    nombre: string;
+    poder: string;
+    edad: number;
+    pasatiempo: string;
+
+    constructor() { }
+
+    usarPoder() {}
+
+    volar() {}
+
+    correr() {}
+
+    caminar() {}
+
+    revivir() {}
+
+}
